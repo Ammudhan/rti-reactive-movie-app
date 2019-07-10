5c8099d512e005487775cfa77b9b5473240636e2
aravindanjayasekaran <aravindan1436@gmail.com>
2019-07-10 16:04:14 -0400

    [Component testing]

--- src/components/movie-card/movie-card.test.js
+++ src/components/movie-card/movie-card.test.js
@@ -1,13 +1,70 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import renderer, { create } from "react-test-renderer";
 import MovieCard from "./movie-card";
+import MovieService from "../../services/movieService";
 
 describe("Movie Card Component", () => {
 
-    it("should define the state as expected", () => {
-        const component = create(<MovieCard />);
+    it("should define the state as expected", async () => {
+        const movie = {
+            id: 429617,
+            original_title: "Spider-Man: Far from Home",
+            poster_path: "/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg",
+            overview: "Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.",
+            release_date: "2019-06-28",
+            title: "Spider-Man: Far from Home",
+            vote_average: 7.8
+        };
+        const props = {
+            location: {
+                state: {
+                    movieId: 429617
+                }
+            }
+        };
+        const mockData = {
+            data: movie
+        };
+        const mock = jest.spyOn(MovieService, "getMovieById");
+        const promise = Promise.resolve(mockData);
+        mock.mockImplementation(() => promise);
+        const component = create(<MovieCard {...props}/>);
+        await promise;
+        expect(mock).toHaveBeenCalled();
         const instance = component.getInstance();
+        expect(instance.state.movie).toBe(movie);
         expect(instance.state.error).toBe('');
+    });
+
+    it('should matches snapshot', async () => {
+        const movie = {
+            id: 429617,
+            original_title: "Spider-Man: Far from Home",
+            poster_path: "/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg",
+            overview: "Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.",
+            release_date: "2019-06-28",
+            title: "Spider-Man: Far from Home",
+            vote_average: 7.8
+        };
+        const props = {
+            location: {
+                state: {
+                    movieId: 429617
+                }
+            }
+        };
+        const mockData = {
+            data: movie
+        };
+        const mock = jest.spyOn(MovieService, "getMovieById");
+        const promise = Promise.resolve(mockData);
+        mock.mockImplementation(() => promise);
+
+        const component = renderer.create(<MovieCard {...props}/>);
+        await promise;
+
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     });
 
 });
--- src/components/movie-list/movie-list.test.js
+++ src/components/movie-list/movie-list.test.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import renderer, { create } from "react-test-renderer";
 import MovieList from "./movie-list";
+import MovieService from "../../services/movieService";
+import {MemoryRouter} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 describe("Movie List Component", () => {
 
     it("should define the state as expected", async () => {
-        const component = create(<MovieList/>);
-        const instance = component.getInstance();
-        expect(instance.state.error).toBe('');
+        const mockData = {
+           data: {
+                results: [
+                    {
+                        id: 429617,
+                        original_title: "Spider-Man: Far from Home",
+                        poster_path: "/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg"
+                    }
+                ]
+            }
+        };
+        const mock = jest.spyOn(MovieService, "getMovies");
+        const promise = Promise.resolve(mockData);
+        mock.mockImplementation(() => promise);
+        create(
+            <MemoryRouter>
+                <MovieList/>
+            </MemoryRouter>
+        );
+        await promise;
+        expect(mock).toHaveBeenCalled();
     });
 
 });
